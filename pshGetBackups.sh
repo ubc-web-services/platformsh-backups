2048cebe0ee83b50e70bb7712da3f76693533d4c
Sean K <sean.killen@ubc.ca>
2018-06-22 10:19:03 -0700

    temp remove bits

--- pshGetBackups.sh
+++ pshGetBackups.sh
@@ -17,19 +17,19 @@ if [ ! -d $backups_folder/$1/$d/private ]; then
   mkdir -p $backups_folder/$1/$d/private;
 fi
 
-platform db:dump --gzip -y -t -f db_dump-$l.sql.gz --directory $backups_folder/$1/$d --project $2 --environment master
-echo "DB backed up..."
+#platform db:dump --gzip -y -t -f db_dump-$l.sql.gz --directory $backups_folder/$1/$d --project $2 --environment master
+#echo "DB backed up..."
 
 root=$(platform --project=$2 --environment=master --property=web.locations./.root app:config-get)
 
 platform --project=$2 --environment=master --mount=$root/sites/default/files --target=$backups_folder/$1/$d/files --yes mount:download
 tar -cvzf $backups_folder/$1/$d/files-$l.tar.gz $backups_folder/$1/$d/files
-rm -rf $backups_folder/$1/$d/files
-echo "Files backed up..."
+#rm -rf $backups_folder/$1/$d/files
+echo "Public Files backed up..."
 
-platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
-tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
-rm -rf $backups_folder/$1/$d/private
-echo "Private files backed up..."
+#platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
+#tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
+#rm -rf $backups_folder/$1/$d/private
+#echo "Private files backed up..."
 
 #exit
