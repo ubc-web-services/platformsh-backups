0f86b3386e0b87462b24203d962af05ce75fab1e
Sean K <sean.killen@ubc.ca>
2018-06-22 11:15:53 -0700

    fix typo

--- pshGetBackups.sh
+++ pshGetBackups.sh
@@ -28,7 +28,7 @@ root=$(platform --project=$2 --environment=master --property=web.locations./.roo
 #echo "Public Files backed up..."
 
 platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
-ar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
+tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
 rm -rf $backups_folder/$1/$d/private
 echo "Private files backed up..."
 
