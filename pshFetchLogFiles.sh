33a7ba57eb69535ca30fb6cb013eb9c9c0ebb9b0
Sean K <sean.killen@ubc.ca>
2018-07-06 14:18:31 -0700

    add other log files

--- pshFetchLogFiles.sh
+++ pshFetchLogFiles.sh
@@ -23,17 +23,20 @@ do
 
 #	BUILD_ID=dontKillMe bash -ex pshGetBackups.sh "${NAME}" "${PROJECT_ID}" &
 
-	echo "getting platform log for ${NAME} ${PROJECT_ID}"
-	echo "destination $logs_folder/${NAME} "
-
-
+	echo "getting platform logs for ${NAME} ${PROJECT_ID}"
+	
 	if [ ! -d $logs_folder/${NAME} ]; then
 	  mkdir -p $logs_folder/${NAME};
 	fi
+	echo "destination $logs_folder/${NAME} "
 
-	#if [ -n "${NAME}" ]
-	#then
+	#TODO run in pipeline instead?
  	platform log access --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/access.log &
-	#fi
-
+	platform log app --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/app.log &
+	platform log cron --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/cron.log &
+	platform log deploy --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/deploy.log &
+	platform log error --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/error.log &
+	platform log php.access --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/php.access.log &
+	platform log post-deploy --project=${PROJECT_ID}  --environment=master > $logs_folder/${NAME}/post-deploy.log &
+	
 done < ID_LIST_RAW
