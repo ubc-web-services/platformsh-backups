87ae651e5aadbc314efa2d2f4ba98d202901dc49
Sean K <sean.killen@ubc.ca>
2018-08-21 10:29:49 -0700

    comment out private files

--- pshGetBackups.sh
+++ pshGetBackups.sh
@@ -27,9 +27,9 @@ tar -cvzf $backups_folder/$1/$d/files-$l.tar.gz $backups_folder/$1/$d/files
 rm -rf $backups_folder/$1/$d/files
 echo "$1 Public Files backed up..."
 
-platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
-tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
-rm -rf $backups_folder/$1/$d/private
-echo "$1 Private files backed up..."
+#platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
+#tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
+#rm -rf $backups_folder/$1/$d/private
+#echo "$1 Private files backed up..."
 
 #exit
