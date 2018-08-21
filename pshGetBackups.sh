25ca5b3ce78a86685e4167395ad272b6c944e15a
Sean K <sean.killen@ubc.ca>
2018-08-21 11:41:02 -0700

    comment out files

--- pshGetBackups.sh
+++ pshGetBackups.sh
@@ -22,14 +22,14 @@ echo "$1 DB backed up..."
 
 root=$(platform --project=$2 --environment=master --property=web.locations./.root app:config-get)
 
-platform --project=$2 --environment=master --mount=$root/sites/default/files --target=$backups_folder/$1/$d/files --yes mount:download
-tar -cvzf $backups_folder/$1/$d/files-$l.tar.gz $backups_folder/$1/$d/files
-rm -rf $backups_folder/$1/$d/files
-echo "$1 Public Files backed up..."
+#platform --project=$2 --environment=master --mount=$root/sites/default/files --target=$backups_folder/$1/$d/files --yes mount:download
+#tar -cvzf $backups_folder/$1/$d/files-$l.tar.gz $backups_folder/$1/$d/files
+#rm -rf $backups_folder/$1/$d/files
+#echo "$1 Public Files backed up..."
 
-platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
-tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
-rm -rf $backups_folder/$1/$d/private
-echo "$1 Private files backed up..."
+#platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
+#tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
+#rm -rf $backups_folder/$1/$d/private
+#echo "$1 Private files backed up..."
 
 exit
