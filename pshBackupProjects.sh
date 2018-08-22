a0e4aa8703dfbd022ac47b9006721e576a0ed302
Sean K <sean.killen@ubc.ca>
2018-08-22 10:50:12 -0700

    remove amp

--- pshBackupProjects.sh
+++ pshBackupProjects.sh
@@ -52,7 +52,7 @@ do
 	
 	#BUILD_ID=dontKillMe platform db:dump --gzip -y -t -f db_dump-${NAME}.sql.gz --directory $backups_folder/${NAME}/$d --project ${PROJECT_ID} --environment master &
 
-	BUILD_ID=dontKillMe bash -ex pshGetBackups.sh "${NAME}" "${PROJECT_ID}" &
+	BUILD_ID=dontKillMe bash -ex pshGetBackups.sh "${NAME}" "${PROJECT_ID}"
 
 done < ID_LIST_RAW
 
