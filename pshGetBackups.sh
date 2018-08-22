4ac034334118926f47a6e62112d6ffc7503f39c9
Sean K <sean.killen@ubc.ca>
2018-08-22 10:48:47 -0700

    add files

--- pshGetBackups.sh
+++ pshGetBackups.sh
@@ -23,10 +23,11 @@ echo "$1 DB backed up..." >> out.txt
 
 root=$(platform --project=$2 --environment=master --property=web.locations./.root app:config-get)
 
-#platform --project=$2 --environment=master --mount=$root/sites/default/files --target=$backups_folder/$1/$d/files --yes mount:download
-#tar -cvzf $backups_folder/$1/$d/files-$l.tar.gz $backups_folder/$1/$d/files
-#rm -rf $backups_folder/$1/$d/files
-#echo "$1 Public Files backed up..."
+platform --project=$2 --environment=master --mount=$root/sites/default/files --target=$backups_folder/$1/$d/files --yes mount:download
+tar -cvzf $backups_folder/$1/$d/files-$l.tar.gz $backups_folder/$1/$d/files
+rm -rf $backups_folder/$1/$d/files
+
+echo "$1 Public Files backed up..." >> out.txt
 
 #platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/$d/private --yes mount:download
 #tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/$d/private
