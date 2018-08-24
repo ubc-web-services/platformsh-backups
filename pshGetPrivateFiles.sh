4c6a7d6797775e81f903a7349f1d6deecc37e6f6
Sean Killen <sean.killen@ubc.ca>
2018-08-24 10:56:03 -0700

    add private files

---
+++ pshGetPrivateFiles.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+backups_folder=/ut01/backups
+
+d=$(date +%Y-%m-%d)
+l=$(date +%Y-%m-%dT%H:%M)
+
+# private file master
+if [ ! -d $backups_folder/$1/private ]; then
+  mkdir -p $backups_folder/$1/private;
+fi
+
+root=$(platform --project=$2 --environment=master --property=web.locations./.root app:config-get)
+
+platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/private --yes mount:download
+tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/private
+
+echo "$1 Private files backed up..."
