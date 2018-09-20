85f710b69ff6a36e805f7ee92f39f0fbed4f5b80
Sean Killen <sean.killen@ubc.ca>
2018-09-20 13:50:56 -0700

    add some options to removing old backups

--- pshRemoveOldBackups.sh
+++ pshRemoveOldBackups.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# Need to keep 10 days of backups, plus a weekly backup for 3 months
-# If today is a Monday, archive backups from 10 days ago
-# Delete archive from 90 days ago, if it exists
+# Need to keep x days of backups, plus a weekly backup for x months
+# If today is a Monday, archive backups from x days ago
+# Delete archive from x days ago, if it exists
+
+# set defaults if not passed in
+xdays=${1:-5}
+xmonths=${2:-30}
 
 backups_folder=/ut01/backups
 
 d=$(date +%Y-%m-%d)
 DOW=$(date +%u)
 
-tendaysago=$(date --date "$d -10 days" +%Y-%m-%d)
-threemonthsago=$(date --date "$d -90 days" +%Y-%m-%d)
+daysago=$(date --date "$d -$xdays days" +%Y-%m-%d)
+monthsago=$(date --date "$d -$xmonths days" +%Y-%m-%d)
 
 while IFS= read -r project; 
 do 
-	if [ "$DOW" == 1 ] && [ -d "$backups_folder/$project/$tendaysago" ]
+	if [ "$DOW" == 1 ] && [ -d "$backups_folder/$project/$daysago" ]
 	then 
-		mv $backups_folder/$project/$tendaysago $backups_folder/$project/archive/
-		echo "moved $project backup from $tendaysago to archive"
+		mv $backups_folder/$project/$daysago $backups_folder/$project/archive/
+		echo "moved $project backup from $daysago to archive"
 	else
-		if [ -d "$backups_folder/$project/$tendaysago" ]; then
-			rm -rf $backups_folder/$project/$tendaysago
-			echo "removed $project backup from $tendaysago"
+		if [ -d "$backups_folder/$project/$daysago" ]; then
+			rm -rf $backups_folder/$project/$daysago
+			echo "removed $project backup from $daysago"
 		fi	
 	fi
-		if [ -d "$backups_folder/$project/archive/$threemonthsago" ]; then
-			rm -rf $backups_folder/$project/archive/$threemonthsago
-			echo "removed $project archive from $threemonthsago"
+		if [ -d "$backups_folder/$project/archive/$monthsago" ]; then
+			rm -rf $backups_folder/$project/archive/$monthsago
+			echo "removed $project archive from $monthsago"
 		fi
 done < DIR_LIST.last	
