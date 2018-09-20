d34ebea97644e0c2263abaa9546bdacbd13a8a65
Sean Killen <sean.killen@ubc.ca>
2018-09-20 12:42:32 -0700

    exclude mysql dumps from backup migrate

--- pshGetPrivateFiles.sh
+++ pshGetPrivateFiles.sh
@@ -18,9 +18,9 @@ fi
 root=$(platform --project=$2 --environment=master --property=web.locations./.root app:config-get)
 
 # FILES EXCLUDED
-# mysql,mysql.gz,mysql.gz.info
+# .mysql,.mysql.gz,.mysql.gz.info
 
-platform --project=$2 --environment=master --mount=private --target=$backups_folder/$1/private --exclude=mysql,mysql.gz,mysql.gz.info --yes mount:download
+platform mount:download --project=$2 --environment=master --mount=private --target=$backups_folder/$1/private --exclude=*.mysql --exclude=*.mysql.gz --exclude=*.mysql.gz.info --yes
 tar -cvzf $backups_folder/$1/$d/private-$l.tar.gz $backups_folder/$1/private
 
 echo "$1 Private files backed up..."
